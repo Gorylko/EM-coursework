1320d0b55bf9a4b576384a4c56c972231462edc2
MINSK\Kiryl_Shamiakou <kiryl_shamiakou@epam.com>
2020-11-07 16:11:55 +0300

    Fixed connection string + fixed EmployeeInfo model

--- EM.Data/Entities/EmployeeInfo.cs
+++ EM.Data/Entities/EmployeeInfo.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace EM.Data.Entities
 {
     public class EmployeeInfo
@@ -7,5 +9,10 @@ namespace EM.Data.Entities
         public string Name { get; set; }
         
         public string Value { get; set; }
+        
+        public int EmployeeId { get; set; }
+
+        [ForeignKey((nameof(EmployeeId)))]
+        public Employee Employee { get; set; }
     }
 }
