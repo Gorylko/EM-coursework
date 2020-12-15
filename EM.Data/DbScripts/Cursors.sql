35245e24904cdbdf704bbbc8ca87ae673e6f9c73
MINSK\Kiryl_Shamiakou <kiryl_shamiakou@epam.com>
2020-12-15 19:26:54 +0300

    Updated cursors

--- EM.Data/DbScripts/Cursors.sql
+++ EM.Data/DbScripts/Cursors.sql
@@ -1,10 +1,9 @@
 USE EmDb
 
 DECLARE UserCredCursor CURSOR
-GLOBAL
-SCROLL
-KEYSET
-TYPE_WARNING
+	SCROLL
+	KEYSET
+	TYPE_WARNING
 FOR
 
 SELECT PasswordSalt, [Password], Id
@@ -13,10 +12,9 @@ FROM dbo.Users
 GO
 
 DECLARE RolesCursor CURSOR
-GLOBAL
-SCROLL
-KEYSET
-TYPE_WARNING
+	GLOBAL
+	KEYSET
+	TYPE_WARNING
 FOR
 
 SELECT *
@@ -25,10 +23,9 @@ FROM vwUsersRoles
 GO
 
 DECLARE EmployeesPenaltiesNonArchivedCursor CURSOR
-GLOBAL
-SCROLL
-KEYSET
-TYPE_WARNING
+	GLOBAL
+	SCROLL
+	TYPE_WARNING
 FOR
 
 SELECT *
@@ -37,10 +34,9 @@ FROM vwEmployeesPenaltiesNonArchived
 GO
 
 DECLARE LoginCursor CURSOR
-GLOBAL
-SCROLL
-KEYSET
-TYPE_WARNING
+	GLOBAL
+	SCROLL
+	KEYSET
 FOR
 
 SELECT Id, [Login]
@@ -49,14 +45,44 @@ FROM dbo.Users
 GO
 
 DECLARE EmployeesPremiumsNonArchivedCursor CURSOR
-GLOBAL
-SCROLL
-KEYSET
-TYPE_WARNING
+	GLOBAL
+	SCROLL
+	KEYSET
+	TYPE_WARNING
 FOR
 
 SELECT *
 FROM vwEmployeesPremiumsNonArchived
+
+GO
+
+DECLARE @name VARCHAR(50) -- database name 
+DECLARE @path VARCHAR(256) -- path for backup files 
+DECLARE @fileName VARCHAR(256) -- filename for backup 
+DECLARE @fileDate VARCHAR(20) -- used for file name 
+
+SET @path = 'C:\Backup\' 
+
+SELECT @fileDate = CONVERT(VARCHAR(20),GETDATE(),112) 
+
+DECLARE db_cursor CURSOR FOR 
+SELECT name 
+FROM MASTER.dbo.sysdatabases 
+WHERE name NOT IN ('master','model','msdb','tempdb') 
+
+OPEN db_cursor  
+FETCH NEXT FROM db_cursor INTO @name  
+
+WHILE @@FETCH_STATUS = 0  
+BEGIN  
+      SET @fileName = @path + @name + '_' + @fileDate + '.BAK' 
+      BACKUP DATABASE @name TO DISK = @fileName 
+
+      FETCH NEXT FROM db_cursor INTO @name 
+END 
+
+CLOSE db_cursor  
+DEALLOCATE db_cursor 
 
 GO
 
