4b5ed86aa91095a925f72bb6c0339bebe142db36
Kiryl Shamiakou <Kiryl_Shamiakou@epam.com>
2021-04-04 15:52:22 +0300

    microupdate

--- EM.Web/ClientApp/src/app/pages/employees-list/employees-list.component.ts
+++ EM.Web/ClientApp/src/app/pages/employees-list/employees-list.component.ts
@@ -1,5 +1,7 @@
 import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { tap } from 'rxjs/operators';
 import { EmployeeSource } from '../../data/employee-source';
 import { PageSidebarStateService } from '../../data/page-sidebar-state.service';
@@ -15,6 +17,7 @@ import { SearchListRequest } from '../../services/models/search-list-request';
 export class EmployeesListComponent implements OnInit, AfterViewInit {
 	public pageTitle: string = 'Employeer';
 
+	public searchForm: FormGroup;
 	public dataSource: EmployeeSource;
 	public displayedColumns: string[] = ['name', 'surname', 'edit'];
 	public count: number = 0;
@@ -26,8 +29,13 @@ export class EmployeesListComponent implements OnInit, AfterViewInit {
 	) { }
 
 	@ViewChild(MatPaginator) paginator: MatPaginator;
+	@ViewChild(MatSort) sort: MatSort;
 
 	ngOnInit() {
+		this.searchForm = new FormGroup({
+			search: new FormControl(''),
+		});
+
 		this.dataSource = new EmployeeSource(this.employeesDataService, this.cdr);
 
 		this.dataSource.loadEmployees({
